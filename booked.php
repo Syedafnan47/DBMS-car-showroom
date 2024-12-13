88e54b90341bde2ecd3253c767657fbfe638d7ed
Syedafnan47 <syedtarooq786@gmail.com>
2024-12-13 21:01:20 +0530

    first commit

---
+++ booked.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    
+     <h1> Car is Booked </h1>
+</body>
+</html>
---
+++ dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$db = mysqli_connect("localhost", "root", "Syedafnan78647", "car_showroom");
+
+if (!$db) {
+    die("Could not connect: " . mysqli_connect_error());
+}
+?>
